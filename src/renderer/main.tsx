d5f3985f09902461dddad793e05c637d0fb7626c
yancy <yancyyu.ok@gmail.com>
2026-05-23 00:16:52 +0800

    revert: 恢复原版 App (TabbedLayout)，删除 newapp 目录

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -1,12 +1,30 @@
 import './index.css';
+import 'react-grid-layout/css/styles.css';
+import 'react-resizable/css/styles.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { NewApp } from './newapp/NewApp';
+import { App } from './App';
+import { initSentryRenderer } from './sentry';
+import { initializeNotificationListeners } from './store';
+
+declare global {
+  interface Window {
+    __claudeTeamsUiDidInit?: boolean;
+  }
+}
+
+// Sentry must be initialised before React renders.
+initSentryRenderer();
+
+if (!window.__claudeTeamsUiDidInit) {
+  window.__claudeTeamsUiDidInit = true;
+  initializeNotificationListeners();
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <NewApp />
+    <App />
   </React.StrictMode>
 );
